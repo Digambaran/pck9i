81d2b2e4c527f9c06e76b141bbceb04767d414b5
Digambaran <arjunsreekumar.1@gmail.com>
2023-02-01 09:05:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Pck9i from './pck9i'
+export default function App() {
+  return (
+    <div>
+      <Pck9i/>
+    </div>
+  )
+}
---
+++ src/pck9i.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Pck9i = () => {
+  return (
+   <p>Hello from pck9i </p>
+  )
+}
+export default Pck9i
